210f131bb803208828cd25d1cc2f61e6e429baac
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-11-11 17:48:47 -0500

    Initial Commit

---
+++ project2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project2
+//
+//  Created by Ammaar Iqbal on 11/11/17.
+//  Copyright © 2017 Ammaar Iqbal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
