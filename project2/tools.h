a268b6b32968df6d6fce76390e4febf9fd6d6d42
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-11-11 18:35:03 -0500

    actually made the row struct

--- project2/tools.h
+++ project2/tools.h
@@ -6,26 +6,37 @@
 
 #include "forkTools.h"
 
-//struct row {
-//
-//    char color;
-//    char director_name;
-//    unsigned int num_critic_for_reviews;
-//    unsigned int duration;
-//    unsigned int director_facebook_likes;
-//    unsigned int actor_3_facebook_likes;
-//    char actor_2_name;
-//    unsigned int actor_1_facebook_likes;
-//    unsigned long gross;
-//    char genres;
-//    char actor_1_name;
-//    char movie_title;
-//    unsigned long num_voted_users;
-//    unsigned int cast_total_facebook_likes;
-//    char actor_3_name;
-//    int facenumber_in_poster;
-//    char plot_keywords;
-//};
+struct row {
+
+    char color;
+    char director_name;
+    unsigned int num_critic_for_reviews;
+    unsigned int duration;
+    unsigned int director_facebook_likes;
+    unsigned int actor_3_facebook_likes;
+    char actor_2_name;
+    unsigned int actor_1_facebook_likes;
+    unsigned long gross;
+    char genres;
+    char actor_1_name;
+    char movie_title;
+    unsigned long num_voted_users;
+    unsigned int cast_total_facebook_likes;
+    char actor_3_name;
+    int facenumber_in_poster;
+    char plot_keywords;
+    char movie_imdb_link;
+    unsigned int num_user_for_reviews;
+    char language;
+    char country;
+    char content_rating;
+    double budget;
+    unsigned int title_year;
+    unsigned int actor_2_facebook_likes;
+    double imdb_score;
+    double aspect_ratio;
+    unsigned int movie_facebook_likes;
+};
 
 unsigned int tokenizeRow(const char * line, char * ** row);
 void trim (char * str);
