7374d5827b0bef94c796a33fd0eccaa53bf8097f
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-11-12 14:35:07 -0500

    commented struct row

--- project2/tools.h
+++ project2/tools.h
@@ -6,37 +6,37 @@
 
 #include "forkTools.h"
 
-struct row {
-
-    char * color;
-    char * director_name;
-    double num_critic_for_reviews;
-    double duration;
-    double director_facebook_likes;
-    double actor_3_facebook_likes;
-    char * actor_2_name;
-    double actor_1_facebook_likes;
-    double gross;
-    char * genres;
-    char * actor_1_name;
-    char * movie_title;
-    double num_voted_users;
-    double cast_total_facebook_likes;
-    char * actor_3_name;
-    double facenumber_in_poster;
-    char * plot_keywords;
-    char * movie_imdb_link;
-    double num_user_for_reviews;
-    char * language;
-    char * country;
-    char * content_rating;
-    double budget;
-    double title_year;
-    double actor_2_facebook_likes;
-    double imdb_score;
-    double aspect_ratio;
-    double movie_facebook_likes;
-};
+//struct row {
+//
+//    char * color;
+//    char * director_name;
+//    double num_critic_for_reviews;
+//    double duration;
+//    double director_facebook_likes;
+//    double actor_3_facebook_likes;
+//    char * actor_2_name;
+//    double actor_1_facebook_likes;
+//    double gross;
+//    char * genres;
+//    char * actor_1_name;
+//    char * movie_title;
+//    double num_voted_users;
+//    double cast_total_facebook_likes;
+//    char * actor_3_name;
+//    double facenumber_in_poster;
+//    char * plot_keywords;
+//    char * movie_imdb_link;
+//    double num_user_for_reviews;
+//    char * language;
+//    char * country;
+//    char * content_rating;
+//    double budget;
+//    double title_year;
+//    double actor_2_facebook_likes;
+//    double imdb_score;
+//    double aspect_ratio;
+//    double movie_facebook_likes;
+//};
 
 int tokenizeRow(char * line, char ** row);
 void trim (char * str);
