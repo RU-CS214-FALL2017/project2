1c7c8968c6d4da8e1f001e37bc6c7bc51d9cc03b
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-11-29 09:42:01 -0500

    re ordered for less code

--- project2/mainTools.c
+++ project2/mainTools.c
@@ -2,11 +2,45 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "mainTools.h"
+// Returns the value of the flag at <flagIndex> of <argv> with
+// <argc> elements. Returns NULL if the value is not found.
+char * getFlagValueHelper(int flagIndex, int argc, char ** argv) {
+    
+    if ((flagIndex + 1) >= argc) {
+        return NULL;
+        
+    } else {
+        return argv[flagIndex + 1];
+    }
+}
 
-char * getFlagValue(const char * flag, int argc, char ** argv);
-char * getFlagValueHelper(int flagIndex, int argc, char ** argv);
-int findFlag(const char * flag, int argc, char ** argv);
+// Returns the index of <flag> in <argv> with <argc> elements.
+// Returns -1 if <flag> not found.
+int findFlag(const char * flag, int argc, char ** argv) {
+    
+    for (int i = 0; i < argc; i++) {
+        
+        if (!strcmp(flag, argv[i])) {
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
+// Returns the value of <flag> in <argv> with <argc> elements.
+// Returns NULL if the flag value is not found.
+char * getFlagValue(const char * flag, int argc, char ** argv) {
+    
+    int flagIndex = findFlag(flag, argc, argv);
+    
+    if (flagIndex == -1) {
+        return NULL;
+        
+    } else {
+        return getFlagValueHelper(flagIndex, argc, argv);
+    }
+}
 
 // Returns the column header to sort by. If <argv> doesn't
 // have a specified column header, the functions prints an
@@ -33,44 +67,4 @@ char * getInputDirectory(int argc, char ** argv) {
 // Returns the output directory if found in <argv>, else returns NULL.
 char * getOutputDirectory(int argc, char ** argv) {
     return getFlagValue("-o", argc, argv);
-}
-
-// Returns the value of <flag> in <argv> with <argc> elements.
-// Returns NULL if the flag value is not found.
-char * getFlagValue(const char * flag, int argc, char ** argv) {
-    
-    int flagIndex = findFlag(flag, argc, argv);
-    
-    if (flagIndex == -1) {
-        return NULL;
-        
-    } else {
-        return getFlagValueHelper(flagIndex, argc, argv);
-    }
-}
-
-// Returns the value of the flag at <flagIndex> of <argv> with
-// <argc> elements. Returns NULL if the value is not found.
-char * getFlagValueHelper(int flagIndex, int argc, char ** argv) {
-    
-    if ((flagIndex + 1) >= argc) {
-        return NULL;
-        
-    } else {
-        return argv[flagIndex + 1];
-    }
-}
-
-// Returns the index of <flag> in <argv> with <argc> elements.
-// Returns -1 if <flag> not found.
-int findFlag(const char * flag, int argc, char ** argv) {
-    
-    for (int i = 0; i < argc; i++) {
-        
-        if (!strcmp(flag, argv[i])) {
-            return i;
-        }
-    }
-    
-    return -1;
 }
